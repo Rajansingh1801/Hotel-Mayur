111eb37680580fb579890c0f1f8e6d2b1558c38e
Rajan singh <87349777+Rajansingh1801@users.noreply.github.com>
2023-06-01 10:22:09 +0530

    r1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // document.addEventListener("DOMContentLoaded", function () {
 //   window.addEventListener("scroll", function () {
-//     if (window.scrollY > 300) {
+//     if (window.scrollY > 0) {
 //       document.getElementById("navbar_top").classList.add("fixed-top");
 //       navbar_height = document.querySelector(".navbar").offsetHeight;
 //       document.body.style.paddingTop = navbar_height + "px";
